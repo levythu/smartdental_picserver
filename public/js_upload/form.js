8a68c0a83409e93537f26dd852254474606d84c1
Levy <zly.george@163.com>
2014-12-27 20:53:48 +0800

    增加了选牙操作和对数据域的判定操作

---
+++ public/js_upload/form.js
@@ -0,0 +1,40 @@
+$(document).ready(function()
+{
+    $("#input-tooth").html("");
+    for (var i=1;i<=32;i++)
+    {
+        $("#input-tooth")[0].innerHTML+="<option value="+i+">"+i+"</option>";
+    }
+    $("#submitter").click(function()
+    {
+        if ($("#input-uid")[0].value=="")
+        {
+            alert("请输入一个用户id");
+            return false;
+        }
+
+        if ($("#input-name")[0].value=="")
+        {
+            alert("请输入一个病历名称");
+            return false;
+        }
+
+        if ($("#input-area")[0].value=="")
+        {
+            alert("请输入该病理图片的病患区域");
+            return false;
+        }
+
+        if ($("#input-diagnose")[0].value=="")
+        {
+            alert("请输入该病理图片的诊断结果");
+            return false;
+        }
+
+        if ($("#input-tooth")[0].value=="")
+        {
+            alert("请输入该病理图片的牙齿编号");
+            return false;
+        }
+    });
+});
